1626e82c16d06f0f32a14f77dc06dcea266460eb
MG <mmoroane@hotmail.com>
2023-02-20 14:04:01 +0200

    Linked frontend to backend

--- Customers-Angular/src/app/customers/customers.component.ts
+++ Customers-Angular/src/app/customers/customers.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Customers, HttpClientService } from '../service/http-client.service'; 
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.css']
 })
-export class CustomersComponent {
 
+export class CustomersComponent implements OnInit {
+  customers: Customers[] = [];
+
+  constructor(private httpClient: HttpClientService) {
+  }
+
+  ngOnInit(): void {
+    this.httpClient.getCustomers().subscribe(
+      response => {this.customers = response;}
+    )
+  }
 }
--- Customers-Angular/src/app/service/http-client.service.ts
+++ Customers-Angular/src/app/service/http-client.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export class Customers {
+  constructor(public firstName: string, public lastName: string) {}
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpClientService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getCustomers() {
+    console.log("test call");
+    return this.httpClient.get<Customers[]>('http://localhost:8080/Customers');
+  }
 }
